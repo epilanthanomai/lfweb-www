723e45455908820d098bb2f32f2f2c18f80bb5d6
Hope Ranker <hranker@groupcse.com>
2015-12-16 17:54:06 -0500

    Package build for distribution

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
 gulp = require("gulp");
+
+// css processing
 postcss = require("gulp-postcss");
 cssnext = require('cssnext');
 cssnano = require('gulp-cssnano');
+
+// packaging & util
+tar = require('gulp-tar');
+gzip = require('gulp-gzip');
 clean = require('gulp-clean');
 
+pkg = require('./package.json');
+
+// paths
 src_dir = './src';
 build_root = './build';
-build_dir = build_root + '/lfsite-www';
+build_name = pkg.name + '-' + pkg.version;
+build_dir = build_root + '/' + build_name;
+docroot = build_dir + '/docroot';
 
-gulp.task('default', ['static', 'css']);
+dist_root = './dist';
+
+// tasks
+
+gulp.task('default', ['package']);
+gulp.task('build', ['static', 'css']);
 
 gulp.task('static', function() {
   var static_src = [
@@ -16,7 +32,7 @@ gulp.task('static', function() {
     src_dir + '/*.png'
   ];
   return gulp.src(static_src)
-      .pipe(gulp.dest(build_dir));
+      .pipe(gulp.dest(docroot));
 });
 
 gulp.task('css', function() {
@@ -26,10 +42,17 @@ gulp.task('css', function() {
   return gulp.src(src_dir + "/*.css")
       .pipe(postcss(processors))
       .pipe(cssnano())
-      .pipe(gulp.dest(build_dir));
+      .pipe(gulp.dest(docroot));
+});
+
+gulp.task('package', ['build'], function() {
+  return gulp.src(build_dir + '/**/*', { base: build_root })
+      .pipe(tar(build_name + '.tar'))
+      .pipe(gzip())
+      .pipe(gulp.dest(dist_root));
 });
 
 gulp.task('clean', function() {
-  return gulp.src(build_root)
+  return gulp.src([build_root, dist_root])
       .pipe(clean());
 });
